11447a64457bc7f9af806fbfda6fb5727bf2c512
diego192599 <di.cramire@udeo.edu.gt>
2025-08-06 16:34:08 -0600

    creacion de clase y implementacion de raise para detener el programa en caso de ingresar un valor no valido

--- main.py
+++ main.py
@@ -1,1 +1,7 @@
-print("Menu")
+class Repartidor:
+    def __init__(self,nombre,paquetes,zona):
+        if not nombre or not  zona or paquetes<0:
+            raise ValueError("Datos no validos")
+        self.nombre=nombre
+        self.paquetes=paquetes
+        self.zona=zona

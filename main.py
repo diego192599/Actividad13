404b834be9869e17c4c0d6d7576da96187dcbb75
diego192599 <di.cramire@udeo.edu.gt>
2025-08-06 16:47:25 -0600

    creacion de metodo de verificacion de existencia de nombrs

--- main.py
+++ main.py
@@ -5,3 +5,21 @@ class Repartidor:
         self.nombre=nombre
         self.paquetes=paquetes
         self.zona=zona
+    def __str__(self):
+        return f"{self.nombre} -{self.paquetes} paquetes - Zona:{self.zona}"
+
+class EmpresaMensjeria:
+    def __init__(self):
+        self.repartidores=[]
+    def existeNombre(self,nombre):
+        for r in self.repartidores:
+            if r.nombre.lower()==nombre.lower():
+                return True
+        return False
+    def agregar(self,repartidor):
+        if self.existeNombre(repartidor.nombre):
+            print("Este nombre ya esta registrado")
+            return
+        self.repartidores.append(repartidor)
+
+
